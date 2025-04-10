bf4af3642ff205151c04bdc04a6628bcae48d9db
yowon08 <jootvpps@gmail.com>
2025-04-10 21:18:57 +0900

    ㅇㅇ

--- app.py
+++ app.py
@@ -39,3 +39,6 @@ def get_notion():
             ))
 
     return jsonify({"blocks": texts})
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render가 요구하는 방식!
+    app.run(host="0.0.0.0", port=port)
